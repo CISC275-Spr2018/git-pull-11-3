4ab6dba7d8bf9380b18dd1a3573e6fdb0cf9698a
rlyshw <rlyshw@gmail.com>
2018-03-05 18:04:36 -0500

    Created model to encapsulate the orc movement data

--- Model.java
+++ Model.java
@@ -8,3 +8,40 @@
  * provide direction
  * provide location
  **/
+
+class Model {
+  private int width, height, imageWidth, imageHeight;
+  private int x=0;
+  private int y=0;
+  private int xDir = 1;
+  private int yDir = 1;
+  private final int xIncr = 8;
+  private final int yIncr = 2;
+
+  Model(int width, int height, int imageWidth, int imageHeight){
+    this.width = width;
+    this.height = height;
+    this.imageWidth = imageWidth;
+    this.imageHeight = imageHeight;
+  }
+  public int getX(){
+    return this.x;
+  }
+  public int getY(){
+    return this.y;
+  }
+  public int[] getDirect(){
+    int[] dir = {this.xDir, this.yDir};
+    return dir;
+  }
+
+
+  public void updateLocationAndDirection(){
+    if((this.getX()+this.imageWidth>this.width) || this.getX()<0)
+      this.xDir *= -1;
+    if((this.getY()+this.imageHeight>this.height) || this.getY()<0)
+      this.yDir *= -1;
+    this.x+=xIncr*this.xDir;
+    this.y+=yIncr*this.yDir;
+  }
+}
