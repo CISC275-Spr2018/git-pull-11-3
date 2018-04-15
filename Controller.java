86b1d490d5be8e5fbd62956c7c6dd96ab6d84a50
evancaplan <ecaplan@udel.edu>
2018-04-15 10:16:34 -0400

    Updated controller to take new KeyListener to add to view

--- Controller.java
+++ Controller.java
@@ -15,49 +15,49 @@ import javax.swing.Timer;
  **/
 public class Controller implements ActionListener{
 
-	private static Model model;
-	private static View view;
-	private final static int DRAWDELAY = 50;
-	private static boolean updateFlag= true;
-	JButton button = new JButton("Toggle");
-	ModelUpdateLogic mul; // Arvin: needed to give the Controller a ModelUpdateLogic 
-	
-	//literally just a clock to count the game ticks
-	//increments every time the model and view are updated
-	private int tick_counter = 0;
-	
-	
-	public Controller(){
-		button.setSize(20,20);
-		//button.addActionListener(new ButtonClickHandler(model));
+		private static Model model;
+		private static View view;
+		private final static int DRAWDELAY = 50;
+		private static boolean updateFlag= true;
+		JButton button = new JButton("Toggle");
+		ModelUpdateLogic mul; // Arvin: needed to give the Controller a ModelUpdateLogic 
 		
-		view = new View(button);
-		model = new Model(view.getWidth(), view.getHeight(), view.getImageWidth(), view.getImageHeight());
-		view.button.addActionListener(new ButtonClickHandler(model));
-		mul = new ModelUpdateLogic(model); // Arvin: create a new ModelUpdateLogic and give it the Model
+		//literally just a clock to count the game ticks
+		//increments every time the model and view are updated
+		private int tick_counter = 0;
 		
 		
-		Timer t = new Timer(DRAWDELAY, this);
-		t.start();
+		public Controller(){
+			button.setSize(20,20);
+			//button.addActionListener(new ButtonClickHandler(model));
+			
+			view = new View(button);
+			model = new Model(view.getWidth(), view.getHeight(), view.getImageWidth(), view.getImageHeight());
+			view.button.addActionListener(new ButtonClickHandler(model));
+			mul = new ModelUpdateLogic(model); // Arvin: create a new ModelUpdateLogic and give it the Model
+			
+			
+			Timer t = new Timer(DRAWDELAY, this);
+			t.start();
+		}
+		
+		
+		//the timer calls this method after each DRAWDELAY
+		public void actionPerformed(ActionEvent e) {
+			//model.updateLocationAndDirection(tick_counter);
+			mul.updateLocationAndDirection(tick_counter); // Arvin: updates called by the ModelUpdateLogic class instead of the Model itself
+			view.update(model);
+			tick_counter+=1;
+		}
+		 
+		public static void main(String[] args){
+			EventQueue.invokeLater(new Runnable(){
+				public void run(){
+					
+					new Controller();
+					view.addKeyListener(new KeyListener(model));
+					view.setVisible(true);
+				}
+			});
+		}
 	}
-	
-	
-	//the timer calls this method after each DRAWDELAY
-	public void actionPerformed(ActionEvent e) {
-		//model.updateLocationAndDirection(tick_counter);
-		mul.updateLocationAndDirection(tick_counter); // Arvin: updates called by the ModelUpdateLogic class instead of the Model itself
-		view.update(model);
-		tick_counter+=1;
-	}
-	 
-	public static void main(String[] args){
-		EventQueue.invokeLater(new Runnable(){
-			public void run(){
-				
-				new Controller();
-				view.addKeyListener(model);
-				view.setVisible(true);
-			}
-		});
-	}
-}
